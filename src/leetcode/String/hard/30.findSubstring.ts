a8f6b3f627f00c322c13eb3058cc2961d771e415
许鑫 <xuxin@cloudcc.com>
2024-04-17 18:57:45 +0800

    代码提交 String Hard 30.串联所有单词的子串(findSubstring) 2024-04-17

--- src/leetcode/String/hard/30.findSubstring.ts
+++ src/leetcode/String/hard/30.findSubstring.ts
@@ -7,21 +7,18 @@ function findSubstring(s: string, words: string[]): number[] {
   const wordItemLen = words[0].length,
     wordsStringLen = wordItemLen * words.length,
     sLen = s.length
-  console.log(helpMap)
-  for (let i = 0; i < sLen - wordsStringLen; i++) {
+
+  for (let i = 0; i <= sLen - wordsStringLen; i++) {
     const curStringWordsObj = {} as Record<string, number>
-    // const str = s.slice(i, wordsStringLen)
     let j = i
-    for (; j <= i + wordsStringLen; j += wordItemLen) {
+    for (; j <= i + wordsStringLen - wordItemLen; j += wordItemLen) {
       const item = s.slice(j, j+wordItemLen)
-      // console.log(item, helpMap[item])
       if (!helpMap[item]) {
         break
       }
       curStringWordsObj[item] = (curStringWordsObj[item] || 0) + 1
     }
 
-    console.log(i, curStringWordsObj)
     if (j === i + wordsStringLen) {
       let temp = true
       for (let key in helpMap) {
@@ -43,4 +40,5 @@ function findSubstring(s: string, words: string[]): number[] {
 // console.log(
 //   findSubstring('wordgoodgoodgoodbestword', ['word', 'good', 'best', 'word'])
 // ) //[]
-console.log(findSubstring('barfoofoobarthefoobarman', ['bar', 'foo', 'the'])) //[6,9,12]
+// console.log(findSubstring('barfoofoobarthefoobarman', ['bar', 'foo', 'the'])) //[6,9,12]
+console.log(findSubstring('wordgoodgoodgoodbestword', ["word","good","best","good"])) //[8]
